c8c69ead0ede760b5e64fefcd414cce35209d0a1
Justin Shaw <realjustinshaw@gmail.com>
2022-05-06 15:00:54 -0700

    pushed recent updates

--- examples/main.rs
+++ examples/main.rs
@@ -1,22 +1,9 @@
-use roogle::collections::linked_list::LinkedList;
+use core::cell::RefCell;
 
 fn main() {
-    let mut list: LinkedList<u32> = LinkedList::new();
-    list.push_back(2);
-    list.push_back(1);
-    list.push_back(4);
-    list.push_back(5);
-    list.push_back(3);
-    println!("list len: {}", list.len());
+    let c = RefCell::new("hello".to_owned());
 
-    println!("unsorted list: {}", list);
-    list.sort(|a, b| a > b);
-    println!("sorted list: {}", list);
+    *c.borrow_mut() = "bonjour".to_owned();
 
-    println!("\nremoving:");
-    println!("{:?}", list.pop_back());
-    println!("{:?}", list.pop_back());
-    println!("{:?}", list.pop_back());
-    println!("{:?}", list.pop_back());
-    println!("{:?}", list.pop_back());
+    assert_eq!(&*c.borrow(), "bonjour");
 }
